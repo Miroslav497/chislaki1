b37a101dfdf459e095660eaaff20dd50e5bae6e0
Miroslav <bitgithar@mail.ru>
2023-09-28 23:06:12 +0300

    tree

--- chisl.cpp
+++ chisl.cpp
@@ -9,8 +9,27 @@ void Swap(int line1, int line2, double** a)
   a[line1] = a[line2];
   a[line2] = tmp;
 }
+void print(double** matrix, int N, int M) {
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++){cout << setw(5) << matrix[i][j];}
+       cout << endl;
+  }
+}
 
-
+void otvet(double** matrix, int N, int M, double *F){
+    F[N-1] = matrix[N-1][M-1];
+    for(int i = N-2; i>-1;i--){
+        double sum = 0;
+        for(int k=i+1; k<N; k++){
+            sum += matrix[i][k] * F[k];
+        }
+        F[i] = matrix[i][N] - sum;
+    }
+    
+    for (int i = 0; i < N; i++){
+        cout << "x" << i+1 << ": " << F[i] << endl;
+    }
+}
 
 int main() {
   const int N = 3, M = 4;
@@ -27,7 +46,6 @@ int main() {
   matrix[1] = new double[cols] {-1.0, -2.0, 2.0, 1.0};
   matrix[2] = new double[cols] {0, 1.0, 1.0, 2.0 };
 
-  //out
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < M; j++) 
         cout << setw(5) << matrix[i][j];
@@ -36,28 +54,38 @@ int main() {
 
   cout << endl << endl;
 
-  int tmp;
-  int i, j, k;
-  for (i = 0; i < N; i++) {
-    tmp = matrix[i][i];
-    if (tmp != 0) {
-      for (j = N; j >= i; j--)
-        matrix[i][j] /= tmp;
+  for (int i = 0; i < N-1; i++) {
+        double tmp = 0;
+        if (matrix[i][i] == 0) {
+            for (int j = i + 1; j < N; j++) {
+                if (matrix[j][i] != 0) {
+                    Swap(i, j, matrix);
+                    break;
+                    
+                }
+            }
+        }
+        
+        for (int k = i + 1; k < N; k++) {
+            tmp = matrix[k][i] / matrix[i][i];
+            for (int j = i; j < M; j++) {
+                matrix[k][j] = matrix[k][j] - matrix[i][j] * tmp;
+            }
+        }
     }
-    else Swap(i, i+1, matrix);
-
-    for (j = i + 1; j < N; j++) {
-        tmp = matrix[j][i];
-        for (k = N;k >= i;k--)
-            matrix[j][k] -= tmp * matrix[i][k];
-    }
+    for (int i = 0; i < N; i++) {
+        double dE = matrix[i][i];
+        for (int j = i; j < M; j++) {
+            matrix[i][j] /= dE;
+        }
+        
   }
+  print(matrix, N, M);
 
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < M; j++)
-      cout << setw(5) << matrix[i][j];
-    cout << endl;
-  }
+  double  *F = new double[N];
+  otvet(matrix,N,M,F);
+  delete [] F;
+  
   delete[] matrix[0];
   delete[] matrix[1];
   delete[] matrix[2];
